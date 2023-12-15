aa2cd0c447312293d53fb84eff260b26e44f42d7
Polister <yyla05@foxmail.com>
2023-12-15 11:08:01 +0800

    init：supported common separator edit（支持普通分隔符的批量修改，用于适配金山表单）

---
+++ src/FilesRenameTools.java
@@ -0,0 +1,72 @@
+import java.io.File;
+
+public class FilesRenameTools {
+    private String oldSeparator;
+    private String newSeparator;
+    private String workPath;
+
+    public FilesRenameTools(String oldSeparator, String newSeparator, String workPath) {
+        this.oldSeparator = oldSeparator;
+        this.newSeparator = newSeparator;
+        this.workPath = workPath;
+    }
+    public String getOldSeparator() {
+        return oldSeparator;
+    }
+
+    public void setOldSeparator(String oldSeparator) {
+        this.oldSeparator = oldSeparator;
+    }
+
+    public String getNewSeparator() {
+        return newSeparator;
+    }
+
+    public void setNewSeparator(String newSeparator) {
+        this.newSeparator = newSeparator;
+    }
+
+    public String getWorkPath() {
+        return workPath;
+    }
+
+    public void setWorkPath(String workPath) {
+        this.workPath = workPath;
+    }
+
+    public void startRename() {
+        File workDirectory = new File(getWorkPath());
+        if (!workDirectory.exists() || !workDirectory.isDirectory()) {
+            throw new RuntimeException("目录不存在");
+        }
+
+        File[] files = workDirectory.listFiles();
+
+        if (files == null)
+            throw new RuntimeException("获取文件失败");
+
+        for (File file : files) {
+            String newFilename = getNewFilename(file);
+            String newFilePath = file.getParent() + "\\" + newFilename;
+            if (!file.renameTo(new File(newFilePath)))
+                System.out.println("重命名" + file.getName() + "失败");
+        }
+    }
+
+    private String getNewFilename(File file) {
+        if (file == null)
+            return "";
+
+        String oldName = file.getName(); // 获取旧名字
+        String[] strings = oldName.split(oldSeparator); // 获取分割后的数组
+
+        StringBuilder newName = new StringBuilder();
+        for (String i : strings) {
+            newName.append(i).append(newSeparator);
+        }
+
+        newName.deleteCharAt(newName.length() - 1);
+
+        return newName.toString();
+    }
+}
