6a9832ad6f1055d3ca2d6760526d140770bf0bb0
Polister <yyla05@foxmail.com>
2023-12-15 11:20:09 +0800

    init：supported common separator edit（支持普通分隔符的批量修改，用于适配金山表单）

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         final String oldSep = "-"; // 旧分隔符
         final String newSep = "_"; // 新分隔符
-        final String workPath = "C:\\Users\\Polister\\Desktop\\22计算机5班_大物作业_第九章\\第九章"; // 工作目录
+        final String workPath = ""; // 工作目录
         FilesRenameTools filesRenameTools = new FilesRenameTools(oldSep, newSep, workPath);
         filesRenameTools.startRename();
     }
